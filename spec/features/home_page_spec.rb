2f8421ab5cab7fb0c6f0e9ee723111ae3ffa2dac
Nick Connor <nickconnor52@gmail.com>
2018-08-13 18:00:28 -0400

    Change the route for home page text to reflect new routing -- weird bug encountered with Capybara not finding selector properties on page

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "Home page", :type => :feature do
   scenario "New user lands on site" do
     2.times { create(:dog) }
 
-    visit "/"
+    visit "/dogs?current_page=1"
 
     expect(page).to have_selector('.dog-photo', count: 2)
     expect(page).to have_selector('.dog-name', count: 2)
