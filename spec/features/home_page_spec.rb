6ae1f3fbfe6bcbeb00ba74eac3ab73eb0417c378
Nick Connor <nickconnor52@gmail.com>
2018-08-13 18:18:28 -0400

    Adjust home page spec to check for the presence of an ad

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -8,6 +8,7 @@ RSpec.feature "Home page", :type => :feature do
 
     expect(page).to have_selector('.dog-photo', count: 2)
     expect(page).to have_selector('.dog-name', count: 2)
+    expect(page).to have_selector('.advertisement')
 
     expect(page).to have_text("Sign in")
     expect(page).to have_text("Sign up")
